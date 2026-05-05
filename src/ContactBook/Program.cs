39f0fe901d15a0f1314d26618e8dac6cce48109f
Axel Algarín <axel10algarin@.com>
2026-05-04 21:29:10 -0400

    Add: ShowWelcomeScreen

--- src/ContactBook/Program.cs
+++ src/ContactBook/Program.cs
@@ -4,11 +4,7 @@ public class Program
 {
     public static void Main()
     {
-        Contact c1 = new Contact();
-        Contact c2 = new Contact("Axel");
-        Contact c3 = new Contact("Axel", "Algarin");
-        Contact c4 = new Contact("Axel", "Algarin", "123-456-7890");
-        Contact c5 = new Contact("Axel", "Algarin", "123-456-7890", "axel10algarin@gmail.com");    
-        Contact c6 = new Contact(lname: "Algarin", phone: "123-456-7890", email:"axel10algarin@gmail.com");
+        var cb = new ContactBook();
+        cb.Start();
     }
 }
